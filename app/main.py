0f6373cc55594d242bf557f6eb31e8e2ed7fa985
katia <@Keyt92>
2023-08-11 09:25:36 +0200

    text

--- app/main.py
+++ app/main.py
@@ -1,12 +1,6 @@
-from faker import Faker
-
-
-def name_faker():
-    faker = Faker("ru-RU")
-    name = faker.name()
-    phone_number = faker.phone_number()
-    print(f"Hello {name}, your phone number {phone_number}.")
+from app.services.read_file import read_file
 
 
 def main():
-    name_faker()
+
+    read_file()
